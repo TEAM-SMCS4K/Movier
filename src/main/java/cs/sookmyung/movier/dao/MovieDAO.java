824494410b0618a9f2ab8886c5b7888859f5b55e
euna <81363864+sung-silver@users.noreply.github.com>
2024-05-21 21:43:23 +0900

    [MERGE] 소셜로그인 기능 DAO 패턴으로 리팩토링 (#21)
    * [REFACTOR] 소셜 로그인 Member model 분리
    
    * [REFACTOR] 소셜 로그인 MemberDAO 분리
    
    * [REFACTOR] 소셜 로그인 DAO 패턴으로 리팩터링
    
    * [SYTLE] DAO 네이밍 형식 통일에 따른 이름 변경
    
    * [FEAT] DAO 예외 발생 시 로깅하도록 설정
    
    * [REFACTOR] MemberDAO에서 예외처리에 따른 서블릿 코드 수정

---
+++ src/main/java/cs/sookmyung/movier/dao/MovieDAO.java
@@ -0,0 +1,81 @@
+package cs.sookmyung.movier.dao;
+
+import cs.sookmyung.movier.config.ConfigLoader;
+import cs.sookmyung.movier.model.Movie;
+import cs.sookmyung.movier.model.MovieReviewInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+
+public class MovieDAO {
+    private static final String JDBC_URL = ConfigLoader.getInstance().getKey("oracle.db.url");
+    private static final String DB_USER = ConfigLoader.getInstance().getKey("oracle.db.username");
+    private static final String DB_PASSWORD = ConfigLoader.getInstance().getKey("oracle.db.password");
+    private static MovieDAO instance;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieDAO.class);
+
+    private MovieDAO() {}
+
+    public static synchronized MovieDAO getInstance() {
+        if (instance == null) {
+            instance = new MovieDAO();
+        }
+        return instance;
+    }
+
+    private Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+    }
+
+    public Movie getMovieById(int movieId) {
+        String sql = "SELECT * FROM movies WHERE movie_id = ?";
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, movieId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return new Movie(
+                            resultSet.getInt("movie_id"),
+                            resultSet.getString("movie_name"),
+                            resultSet.getString("movie_poster_img"),
+                            resultSet.getString("movie_thumbnail_img"),
+                            resultSet.getString("movie_genre"),
+                            resultSet.getDate("movie_release_date"),
+                            resultSet.getInt("movie_running_time"),
+                            resultSet.getString("movie_plot")
+                    );
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            LOGGER.error("영화 상세 정보를 가져오는데 실패했습니다.", e);
+        }
+        return null;
+    }
+
+    public MovieReviewInfo getMovieReviewInfoById(int movieId) {
+        String sql = "SELECT m.movie_id, COUNT(r.review_id) AS reviewCount, AVG(r.review_rating) AS ratingAverage"
+                + " FROM movies m LEFT JOIN reviews r ON m.movie_id = r.movie_id"
+                + " WHERE m.movie_id = ?"
+                + " GROUP BY m.movie_id";
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, movieId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return new MovieReviewInfo(
+                            resultSet.getDouble("ratingAverage"),
+                            resultSet.getInt("reviewCount")
+                    );
+                }
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            LOGGER.error("리뷰와 관련된 영화 정보를 가져오는데 실패했습니다.", e);
+        }
+        return null;
+    }
+}
