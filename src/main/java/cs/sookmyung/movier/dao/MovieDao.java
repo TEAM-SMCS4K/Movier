95f2a554b9a89438342f18419fa0d489a1a0642d
euna <81363864+sung-silver@users.noreply.github.com>
2024-05-21 17:57:20 +0900

    [MERGE] 영화 상세보기 뷰 - 정보 불러오는 기능 구현 (#19)
    * [FIX] 포스터에 맞추어 줄거리 영역 및 리뷰 리스트 영역 정렬
    
    * [ADD] create_movie.sql 추가
    
    * [ADD] create_review.sql 추가
    
    * [FEAT] Movie 모델 구현
    
    * [FEAT] Movie 모델에 리뷰가 100개 이상이면 100+를 반환하는 getter 구현
    
    * [FEAT] Movie에 대한 DAO 구현
    
    * [FEAT] 리뷰와 관련된 영화 상세정보 model 분리
    
    * [FIX] movie table 상영시간 필드 이름 변경
    
    * [FEAT] getMovieReviewInfoById - 리뷰와 관련된 영화 정보 가져오는 로직 분리
    
    * [FEAT] 영화 정보 불러오기 기능 jsp에 적용
    
    * [DEL] 중복되는 navigationBar 컴포넌트 삭제

---
+++ src/main/java/cs/sookmyung/movier/dao/MovieDao.java
@@ -0,0 +1,85 @@
+package cs.sookmyung.movier.dao;
+
+import cs.sookmyung.movier.config.ConfigLoader;
+import cs.sookmyung.movier.model.Movie;
+import cs.sookmyung.movier.model.MovieReviewInfo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MovieDao {
+    private static final String JDBC_URL = ConfigLoader.getInstance().getKey("oracle.db.url");
+    private static final String DB_USER = ConfigLoader.getInstance().getKey("oracle.db.username");
+    private static final String DB_PASSWORD = ConfigLoader.getInstance().getKey("oracle.db.password");
+
+    // 싱글톤 인스턴스를 저장할 정적 변수
+    private static MovieDao instance;
+
+    // private 생성자를 통해 외부에서의 인스턴스 생성을 방지
+    private MovieDao() {}
+
+    // 싱글톤 인스턴스를 반환하는 메서드
+    public static synchronized MovieDao getInstance() {
+        if (instance == null) {
+            instance = new MovieDao();
+        }
+        return instance;
+    }
+
+    public Movie getMovieById(int movieId) {
+        Movie movie = null;
+        try  {
+            System.out.println(movieId + "영화 아이디가 제대로 들어감?");
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+            String sql = "SELECT * FROM movies WHERE movie_id =?";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, movieId);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                movie = new Movie(
+                        resultSet.getInt("movie_id"),
+                        resultSet.getString("movie_name"),
+                        resultSet.getString("movie_poster_img"),
+                        resultSet.getString("movie_thumbnail_img"),
+                        resultSet.getString("movie_genre"),
+                        resultSet.getDate("movie_release_date"),
+                        resultSet.getInt("movie_running_time"),
+                        resultSet.getString("movie_plot"));
+            }
+        } catch (Exception e) {
+            System.out.println("영화 상세 정보를 가져오는데 실패했습니다.");
+        }
+        return movie;
+    }
+
+    public MovieReviewInfo getMovieReviewInfoById(int movieId){
+        MovieReviewInfo movieReviewInfo = null;
+        try  {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+            String sql = "SELECT m.movie_id, COUNT(r.review_id) as reviewCount, AVG(r.review_rating) as ratingAverage"
+                    + " FROM movies m LEFT JOIN reviews r ON m.movie_id = r.movie_id "
+                    + " WHERE m.movie_id =?"
+                    + " GROUP BY m.movie_id";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, movieId);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                movieReviewInfo = new MovieReviewInfo(
+                        resultSet.getInt("ratingAverage"),
+                        resultSet.getInt("reviewCount"));
+            }
+        } catch (Exception e) {
+            System.out.println("리뷰와 관련된 영화 정보를 가져오는데 실패했습니다.");
+        }
+        return movieReviewInfo;
+    }
+}
+
