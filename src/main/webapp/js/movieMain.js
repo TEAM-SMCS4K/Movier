4e5ff3b0d0ef0f7a9305aab061ab7ce583c064e9
kwonjeong <kwonjeong9470@gmail.com>
2024-05-26 03:43:04 +0900

    [FEAT] 검색 시 검색 키워드 표시

--- src/main/webapp/js/movieMain.js
+++ src/main/webapp/js/movieMain.js
@@ -37,3 +37,34 @@ function login() {
     profileMenu.style.display = "none";
     window.location.href = "socialLogin.jsp"; // 소셜로그인 페이지
 }
+
+// 검색
+document.addEventListener("DOMContentLoaded", function() {
+    const searchInput = document.getElementById("search_input");
+    const movieListTitle = document.querySelector(".movie_list_title");
+
+    // 검색 실행 함수
+    function performSearch() {
+        const searchQuery = searchInput.value.trim();
+        if (searchQuery !== "") {
+            movieListTitle.textContent = `'${searchQuery}'에 대한 검색결과`;
+            // 검색결과 출력
+        } else {
+            alert("검색어를 입력해주세요.");
+        }
+    }
+
+    // 엔터키 입력 시 검색 실행
+    searchInput.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) { // Enter key code
+            performSearch();
+        }
+    });
+});
+
+
+
+
+
+
+
