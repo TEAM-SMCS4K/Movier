3f222e13d2de5b3fa907c0c9237545494689e0da
kwonjeong <kwonjeong9470@gmail.com>
2024-05-26 03:55:37 +0900

    [Docs] 주석 추가

--- src/main/webapp/js/movieMain.js
+++ src/main/webapp/js/movieMain.js
@@ -48,7 +48,14 @@ document.addEventListener("DOMContentLoaded", function() {
         const searchQuery = searchInput.value.trim();
         if (searchQuery !== "") {
             movieListTitle.textContent = `'${searchQuery}'에 대한 검색결과`;
-            // 검색결과 출력
+            // 검색결과 출력 -> 서버 통신
+            // fetch(`search?query=${searchQuery}`)
+            //     .then(response => response.text())
+            //     .then(data => {
+            //         document.getElementById("movie_list_grid").innerHTML = data;
+            //         document.querySelector(".movie_list_title").textContent = `검색 결과: ${searchQuery}`;
+            //     })
+            //     .catch(error => console.error('Error:', error));
         } else {
             alert("검색어를 입력해주세요.");
         }
