6dea07951e5e121f8912ac4080b12a0398032d93
KwonJeong <48792069+kj9470@users.noreply.github.com>
2024-05-26 16:01:58 +0900

    [FEAT] 검색 시 검색 키워드 표시 기능 구현 (#42)
    * [ADD] 네비바에 profile 태그 id 추가
    
    * [FEAT] 프로필 눌렀을 시 버튼 활성화 기능 구현
    
    * [FEAT] 네비바 눌렀을 시 추가 메뉴 기능 구현
    
    * [FEAT] 네비바 마이페이지 이동 기능 구현
    
    * [FEAT] 네비바 로그인 여부 분기처리
    
    * [FEAT] 검색 시 검색 키워드 표시
    
    * [Docs] 주석 추가
    
    * [Del] 메인 뷰 네비바 기능 삭제
    
    * [FEAT] 네비바에 메뉴 기능 추가 및 프로필 마우스 커서 변경

--- src/main/webapp/js/movieMain.js
+++ src/main/webapp/js/movieMain.js
@@ -1,39 +1,37 @@
+// 검색
 document.addEventListener("DOMContentLoaded", function() {
-    const profile = document.getElementById("profile");
-    const profileMenu = document.getElementById("profile_menu");
+    const searchInput = document.getElementById("search_input");
+    const movieListTitle = document.querySelector(".movie_list_title");
 
-    // 특정 페이지에서만 활성화
-    const specificPageClass = "main-page"; // 해당 클래스가 있는 페이지에서만 작동
-
-    if (document.body.classList.contains(specificPageClass)) {
-        profile.addEventListener("click", function(event) {
-            event.stopPropagation(); // 이벤트 버블링 방지
-            profileMenu.style.display = profileMenu.style.display === "none" || profileMenu.style.display === "" ? "flex" : "none";
-        });
-
-        document.addEventListener("click", function() {
-            profileMenu.style.display = "none";
-        });
-
-        profileMenu.addEventListener("click", function(event) {
-            event.stopPropagation(); // 이벤트 버블링 방지
-        });
+    // 검색 실행 함수
+    function performSearch() {
+        const searchQuery = searchInput.value.trim();
+        if (searchQuery !== "") {
+            movieListTitle.textContent = `'${searchQuery}'에 대한 검색결과`;
+            // 검색결과 출력 -> 서버 통신
+            // fetch(`search?query=${searchQuery}`)
+            //     .then(response => response.text())
+            //     .then(data => {
+            //         document.getElementById("movie_list_grid").innerHTML = data;
+            //         document.querySelector(".movie_list_title").textContent = `검색 결과: ${searchQuery}`;
+            //     })
+            //     .catch(error => console.error('Error:', error));
+        } else {
+            alert("검색어를 입력해주세요.");
+        }
     }
+
+    // 엔터키 입력 시 검색 실행
+    searchInput.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) { // Enter key code
+            performSearch();
+        }
+    });
 });
 
-function goToMyPage() {
-    const profileMenu = document.getElementById("profile_menu");
-    profileMenu.style.display = "none";
-    window.location.href = "myPage.jsp"; // 마이페이지
-}
 
-function logout() {
-    alert("로그아웃합니다.");
-}
 
-function login() {
-    window.location.href = 'login.jsp';
-    const profileMenu = document.getElementById("profile_menu");
-    profileMenu.style.display = "none";
-    window.location.href = "socialLogin.jsp"; // 소셜로그인 페이지
-}
+
+
+
+
