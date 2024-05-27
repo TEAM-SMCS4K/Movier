b3ffd520b582992fe58a70ec7f85beddcfa85a33
YOONJI <jelliijoa@gmail.com>
2024-05-28 00:34:41 +0900

    [MERGE] 리뷰 관련 로직 변경 (#53)

--- src/main/java/cs/sookmyung/movier/dao/ReviewDAO.java
+++ src/main/java/cs/sookmyung/movier/dao/ReviewDAO.java
@@ -1,14 +1,11 @@
 package cs.sookmyung.movier.dao;
 
-import cs.sookmyung.movier.model.MovieReview;
-import cs.sookmyung.movier.model.Review;
+import cs.sookmyung.movier.model.MyPageReview;
 import cs.sookmyung.movier.config.ConfigLoader;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
-import oracle.jdbc.OracleTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,8 +17,7 @@ public class ReviewDAO {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReviewDAO.class);
 
-    private ReviewDAO() {
-    }
+    private ReviewDAO() {}
 
     public static synchronized ReviewDAO getInstance() {
         if (instance == null) {
@@ -35,88 +31,49 @@ public class ReviewDAO {
         return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
     }
 
-//    public List<Review> getReviewsByMemberId(int memberId) throws SQLException {
-//        List<Review> reviews = new ArrayList<>();
-//        String sql = "{ call get_reviews_by_member_id(?, ?) }";
-//        try (Connection connection = getConnection();
-//             CallableStatement cstmt = connection.prepareCall(sql)) {
-//            cstmt.setInt(1, memberId);
-//            cstmt.registerOutParameter(2, Types.REF_CURSOR);
-//            cstmt.execute();
-//
-//            ResultSet rs = (ResultSet) cstmt.getObject(2);
-//            while (rs.next()) {
-//                Review review = new Review(
-//                        rs.getInt("review_id"),
-//                        rs.getString("movie_poster_img"),
-//                        rs.getString("movie_name"),
-//                        rs.getDouble("review_rating"),
-//                        rs.getString("review_content"),
-//                        rs.getDate("review_created_at")
-//                );
-//                reviews.add(review);
-//            }
-//        } catch (ClassNotFoundException e) {
-//            LOGGER.error("Database driver not found", e);
-//            throw new SQLException(e);
-//        }
-//        return reviews;
-//    }
-
-    public List<MovieReview> getSortedReviewsByMovieId(int movieId, double minRating, String option) {
-        List<MovieReview> reviews = new ArrayList<>();
-        SympathyDAO sympathyDAO = SympathyDAO.getInstance();
-        MemberDAO memberDAO = MemberDAO.getInstance();
-        String cs;
-
-        if(option.equals("rating")){
-            cs = getSortedByRatingPLSQL();
-        } else {
-            cs = getSortedByLatestPLSQL();
-        }
-
+    public int getReviewCountByMemberId(int memberId) throws SQLException {
+        int reviewCount = 0;
+        String sql = "{ call get_review_count_by_member_id(?, ?) }";
         try (Connection connection = getConnection();
-             CallableStatement cstmt = connection.prepareCall(cs)) {
+             CallableStatement cstmt = connection.prepareCall(sql)) {
+            cstmt.setInt(1, memberId);
+            cstmt.registerOutParameter(2, Types.INTEGER);
+            cstmt.execute();
+            reviewCount = cstmt.getInt(2);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("Database driver not found", e);
+            throw new SQLException(e);
+        }
+        return reviewCount;
+    }
 
-            cstmt.setInt(1, movieId);
-            cstmt.setDouble(2, minRating);
-            cstmt.registerOutParameter(3, Types.REF_CURSOR);
-
+    public List<MyPageReview> getMyPageReviewsByMemberId(int memberId) throws SQLException {
+        List<MyPageReview> reviews = new ArrayList<>();
+        String sql = "{ call get_reviews_by_member_id(?, ?) }";
+        try (Connection connection = getConnection();
+             CallableStatement cstmt = connection.prepareCall(sql)) {
+            cstmt.setInt(1, memberId);
+            cstmt.registerOutParameter(2, Types.REF_CURSOR);
             cstmt.execute();
 
-            try (ResultSet rs = (ResultSet) cstmt.getObject(3)) {
-                while (rs.next()) {
-                    int review_id = rs.getInt("review_id");
-                    int sympathy_count = sympathyDAO.getSympathyCount(review_id);
-
-                    int reviewer_id = rs.getInt("reviewer_id");
-                    String reviewer_name = memberDAO.getMemberNameById(reviewer_id);
-
-                    MovieReview review = new MovieReview(
-                            review_id,
-                            reviewer_id,
-                            rs.getInt("movie_id"),
-                            rs.getDouble("review_rating"),
-                            rs.getString("review_content"),
-                            rs.getDate("review_created_at"),
-                            reviewer_name,
-                            sympathy_count
-                    );
-                    reviews.add(review);
-                }
+            ResultSet rs = (ResultSet) cstmt.getObject(2);
+            while (rs.next()) {
+                MyPageReview review = new MyPageReview(
+                        rs.getInt("review_id"),
+                        rs.getInt("member_id"),
+                        rs.getInt("movie_id"),
+                        rs.getDouble("review_rating"),
+                        rs.getString("review_content"),
+                        rs.getDate("review_created_at"),
+                        rs.getString("movie_poster_img"),
+                        rs.getString("movie_name")
+                );
+                reviews.add(review);
             }
-        } catch (ClassNotFoundException | SQLException e) {
+        } catch (ClassNotFoundException e) {
             LOGGER.error("Database driver not found", e);
+            throw new SQLException(e);
         }
         return reviews;
     }
-
-    private String getSortedByLatestPLSQL() {
-        return "{ call get_latest_reviews_by_movie_id(?, ?, ?) }";
-    }
-
-    private String getSortedByRatingPLSQL(){
-        return "{ call get_rating_reviews_by_movie_id(?, ?, ?) }";
-    }
-
 }
