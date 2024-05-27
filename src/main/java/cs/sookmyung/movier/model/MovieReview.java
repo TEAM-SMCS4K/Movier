32526f256b2b4faf71ec5ac31610530e855305d3
sung-silver <jinna0319@gmail.com>
2024-05-27 14:10:31 +0900

    [FIX] List 자료형이 아닌 객체에 대하여 이름 변경

--- src/main/java/cs/sookmyung/movier/model/MovieReview.java
+++ src/main/java/cs/sookmyung/movier/model/MovieReview.java
@@ -2,11 +2,11 @@ package cs.sookmyung.movier.model;
 
 import java.util.Date;
 
-public class MovieReviewList extends Review{
+public class MovieReview extends Review{
     private String reviewerName;
     private int sympathyCount;
 
-    public MovieReviewList(int reviewId, int memberId, int movieId, double reviewRating, String reviewContent, Date reviewCreatedAt, String reviewerName, int sympathyCount){
+    public MovieReview(int reviewId, int memberId, int movieId, double reviewRating, String reviewContent, Date reviewCreatedAt, String reviewerName, int sympathyCount){
         super(reviewId, memberId, movieId, reviewRating, reviewContent, reviewCreatedAt);
         this.reviewerName = reviewerName;
         this.sympathyCount = sympathyCount;
