cd8aabb652ed2e3731c1305e34fe68290b99b53d
sung-silver <jinna0319@gmail.com>
2024-05-27 13:34:21 +0900

    [FIX] ReviewList -> MyPageReviewList로 이름 수정

--- src/main/java/cs/sookmyung/movier/model/MyPageReviewList.java
+++ src/main/java/cs/sookmyung/movier/model/MyPageReviewList.java
@@ -2,11 +2,11 @@ package cs.sookmyung.movier.model;
 
 import java.util.Date;
 
-public class ReviewList extends Review {
+public class MyPageReviewList extends Review {
     private String posterImg;
     private String title;
 
-    public ReviewList(int reviewId, int memberId, int movieId, double reviewRating, String reviewContent, Date reviewCreatedAt, String posterImg, String title) {
+    public MyPageReviewList(int reviewId, int memberId, int movieId, double reviewRating, String reviewContent, Date reviewCreatedAt, String posterImg, String title) {
         super(reviewId, memberId, movieId, reviewRating, reviewContent, reviewCreatedAt);
         this.posterImg = posterImg;
         this.title = title;
