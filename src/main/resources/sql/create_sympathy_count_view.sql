9baa8cabb2d673c50d8a885007a770f6f882796d
sung-silver <jinna0319@gmail.com>
2024-05-27 16:42:28 +0900

    [FIX] create_sympathy_count_view 수정

--- src/main/resources/sql/create_sympathy_count_view.sql
+++ src/main/resources/sql/create_sympathy_count_view.sql
@@ -1,4 +1,4 @@
 CREATE VIEW sympathy_count_view AS
-    SELECT review_id, COUNT(*) AS sympathy_count
+    SELECT p_review_id, COUNT(*) AS sympathy_count
     FROM sympathy
-    GROUP BY review_id;
+    GROUP BY p_review_id;
