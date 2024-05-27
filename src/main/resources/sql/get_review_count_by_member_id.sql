91cf250c4f4f4ce8d21da3257f3580f4a53e1131
jelliijoa <jelliijoa@gmail.com>
2024-05-28 00:24:33 +0900

    [FIX] 리뷰 관련 로직 변경

---
+++ src/main/resources/sql/get_review_count_by_member_id.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE PROCEDURE get_review_count_by_member_id (
+    p_member_id IN NUMBER,
+    p_review_count OUT NUMBER
+) AS
+    CURSOR review_cursor IS
+        SELECT review_id
+        FROM reviews
+        WHERE member_id = p_member_id;
+    review_id reviews.review_id%TYPE;
+BEGIN
+    p_review_count := 0;
+
+    OPEN review_cursor;
+    LOOP
+        FETCH review_cursor INTO review_id;
+        EXIT WHEN review_cursor%NOTFOUND;
+        p_review_count := p_review_count + 1;
+    END LOOP;
+    CLOSE review_cursor;
+END;
+/
