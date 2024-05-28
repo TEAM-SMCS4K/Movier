5b373bf0c75f2fa2c0ca0f8438707e788f02a506
sung-silver <jinna0319@gmail.com>
2024-05-28 22:58:16 +0900

    [FEAT] 유저가 해당 리뷰에 공감했는지에 대한 확인용 프로시저 구현

--- src/main/resources/sql/check_sympathy.sql
+++ src/main/resources/sql/check_sympathy.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE PROCEDURE check_sympathy(
-    p_user_id IN sympathy.user_id%TYPE,
-    p_review_id IN sympathy.review_id%TYPE,
+    member_id IN sympathy.p_member_id%TYPE,
+    review_id IN sympathy.p_review_id%TYPE,
     p_result OUT BOOLEAN -- 반환값으로 사용할 OUT 매개변수 추가
 ) AS
     v_review sympathy%ROWTYPE;
 BEGIN
 SELECT * INTO v_review
-FROM user_review_likes
-WHERE user_id = p_user_id AND review_id = p_review_id;
+FROM SYMPATHY
+WHERE  P_MEMBER_ID = member_id AND p_review_id = review_id;
 
-IF v_review.user_id IS NOT NULL AND v_review.review_id IS NOT NULL THEN
+IF v_review.P_MEMBER_ID IS NOT NULL AND v_review.P_REVIEW_ID IS NOT NULL THEN
         p_result := TRUE; -- 공감한 경우
 ELSE
         p_result := FALSE; -- 공감하지 않은 경우
