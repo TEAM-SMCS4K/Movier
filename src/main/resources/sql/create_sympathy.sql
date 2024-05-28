c79b406016d031bb7a8f0b52a4b564ff2a4a1b66
sung-silver <jinna0319@gmail.com>
2024-05-28 23:48:16 +0900

    [FEAT] sympathy 테이블 생성 쿼리에 외래키 제약 조건 추가

--- src/main/resources/sql/create_sympathy.sql
+++ src/main/resources/sql/create_sympathy.sql
@@ -1,3 +1,6 @@
-CREATE TABLE SYMPATHY(
-    p_member_id NUMBER,
-    p_review_id NUMBER);
+CREATE TABLE SYMPATHY (
+                          p_member_id NUMBER,
+                          p_review_id NUMBER,
+                          CONSTRAINT fk_member FOREIGN KEY (p_member_id) REFERENCES members (member_id),
+                          CONSTRAINT fk_review FOREIGN KEY (p_review_id) REFERENCES reviews (review_id)
+);
