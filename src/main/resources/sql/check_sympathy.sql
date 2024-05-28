b70375b22fb7aa6c6186c43740238f1381f3654b
sung-silver <jinna0319@gmail.com>
2024-05-28 23:49:01 +0900

    [FIX] check_sympathy 반환 값을 숫자 타입으로 수정

--- src/main/resources/sql/check_sympathy.sql
+++ src/main/resources/sql/check_sympathy.sql
@@ -1,18 +1,23 @@
 CREATE OR REPLACE PROCEDURE check_sympathy(
     member_id IN sympathy.p_member_id%TYPE,
     review_id IN sympathy.p_review_id%TYPE,
-    p_result OUT BOOLEAN -- 반환값으로 사용할 OUT 매개변수 추가
+    p_result OUT NUMBER -- BOOLEAN 대신 NUMBER 사용
 ) AS
-    v_review sympathy%ROWTYPE;
+    v_sympathy sympathy%ROWTYPE;
 BEGIN
-SELECT * INTO v_review
-FROM SYMPATHY
-WHERE  P_MEMBER_ID = member_id AND p_review_id = review_id;
+    BEGIN
+        SELECT * INTO v_sympathy
+        FROM sympathy
+        WHERE p_member_id = member_id AND p_review_id = review_id;
 
-IF v_review.P_MEMBER_ID IS NOT NULL AND v_review.P_REVIEW_ID IS NOT NULL THEN
-        p_result := TRUE; -- 공감한 경우
-ELSE
-        p_result := FALSE; -- 공감하지 않은 경우
-END IF;
+        IF v_sympathy.p_member_id IS NOT NULL AND v_sympathy.p_review_id IS NOT NULL THEN
+            p_result := 1; -- 공감한 경우 1
+        ELSE
+            p_result := 0; -- 공감하지 않은 경우 0
+        END IF;
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                p_result := 0; -- 공감하지 않은 경우 0
+    END;
 END;
 /
