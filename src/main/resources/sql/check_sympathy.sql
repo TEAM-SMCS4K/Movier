e4cad796931ac71291bbc1b70dae3a54319a98ed
euna <81363864+sung-silver@users.noreply.github.com>
2024-05-29 21:47:36 +0900

    [MERGE] 프로시저, 함수 수정에 따른 소셜 로그인 및 공감 개수 확인 기능 리팩터링, 테이블 제약 조건 추가 (#63)
    * [FEAT] 공감 여부 확인 에러 분기 처리
    
    * [FEAT] 테이블에 not null 제약 조건 및 delete on cascade 추가
    
    * [FEAT] 입력값 검증 추가
    
    * [SYTLE] 들여쓰기 수정
    
    * [FEAT] 입력값 검증 추가
    
    * [REFACTOR] 프로시저 수정에 따른 SympathyDAO - isSympathyExist 리팩토링
    
    * [REFACTOR] 프로시저, 함수 수정에 따른 MovieDAO - getMemberIdBySocialPlatformId, insertMember 리팩토링
    
    * [REFACTOR] MovieDAO - getMemberIdBySocialPlatformId, insertMember 리팩토링에 따른 SocialLoginServlet 리팩토링

--- src/main/resources/sql/check_sympathy.sql
+++ src/main/resources/sql/check_sympathy.sql
@@ -5,19 +5,24 @@ CREATE OR REPLACE PROCEDURE check_sympathy(
 ) AS
     v_sympathy sympathy%ROWTYPE;
 BEGIN
+    -- 입력값 검증
+    IF member_id IS NULL OR member_id <= 0 THEN
+        RAISE_APPLICATION_ERROR(-20003, 'Invalid member_id: ' || TO_CHAR(member_id));
+    ELSIF review_id IS NULL OR review_id <= 0 THEN
+        RAISE_APPLICATION_ERROR(-20004, 'Invalid review_id: ' || TO_CHAR(review_id));
+    END IF;
+    -- 공감 여부 확인
     BEGIN
         SELECT * INTO v_sympathy
         FROM sympathy
         WHERE p_member_id = member_id AND p_review_id = review_id;
+        p_result := 1; -- 공감한 경우 1
 
-        IF v_sympathy.p_member_id IS NOT NULL AND v_sympathy.p_review_id IS NOT NULL THEN
-            p_result := 1; -- 공감한 경우 1
-        ELSE
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
             p_result := 0; -- 공감하지 않은 경우 0
-        END IF;
-        EXCEPTION
-            WHEN NO_DATA_FOUND THEN
-                p_result := 0; -- 공감하지 않은 경우 0
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20002, 'An unexpected error occurred: ' || SQLERRM);
     END;
 END;
 /
--- src/main/resources/sql/insert_member.sql
+++ src/main/resources/sql/insert_member.sql
@@ -5,12 +5,20 @@ CREATE OR REPLACE PROCEDURE insert_member(
     p_member_id OUT NUMBER
 ) AS
 BEGIN
-INSERT INTO members (member_name, kakao_platform_id, member_profile_img)
-VALUES (p_member_name, p_kakao_platform_id, p_member_profile_img)
+    -- 입력값 검증
+    IF p_member_name IS NULL OR LENGTH(TRIM(p_member_name)) = 0 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Invalid member_name: ' || p_member_name);
+    ELSIF p_kakao_platform_id IS NULL OR LENGTH(TRIM(p_kakao_platform_id)) = 0 THEN
+        RAISE_APPLICATION_ERROR(-20003, 'Invalid kakao_platform_id: ' || p_kakao_platform_id);
+    END IF;
+
+    -- 멤버 삽입
+    INSERT INTO members (member_name, kakao_platform_id, member_profile_img)
+    VALUES (p_member_name, p_kakao_platform_id, p_member_profile_img)
     RETURNING member_id INTO p_member_id;
-EXCEPTION
+
+    EXCEPTION
         WHEN OTHERS THEN
-            p_member_id := -1;
-            RAISE_APPLICATION_ERROR(-20002, 'Error inserting member: ' || SQLERRM);
+            RAISE_APPLICATION_ERROR(-20001, 'Error inserting member: ' || SQLERRM);
 END;
 /
