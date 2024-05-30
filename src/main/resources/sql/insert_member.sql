b54ffb083d83698a53b50038ff86784e217810d5
sung-silver <jinna0319@gmail.com>
2024-05-31 00:36:14 +0900

    [CHORE] insert_member - PL/SQL 에러 발생 형식 통일

--- src/main/resources/sql/insert_member.sql
+++ src/main/resources/sql/insert_member.sql
@@ -4,12 +4,14 @@ CREATE OR REPLACE PROCEDURE insert_member(
     p_member_profile_img IN members.member_profile_img%TYPE,
     p_member_id OUT NUMBER
 ) AS
+    INVALID_MEMBER_NAME EXCEPTION;
+    INVALID_KAKAO_PLATFORM_ID EXCEPTION;
 BEGIN
     -- 입력값 검증
     IF p_member_name IS NULL OR LENGTH(TRIM(p_member_name)) = 0 THEN
-        RAISE_APPLICATION_ERROR(-20002, 'Invalid member_name: ' || p_member_name);
+        RAISE INVALID_MEMBER_NAME;
     ELSIF p_kakao_platform_id IS NULL OR LENGTH(TRIM(p_kakao_platform_id)) = 0 THEN
-        RAISE_APPLICATION_ERROR(-20003, 'Invalid kakao_platform_id: ' || p_kakao_platform_id);
+        RAISE INVALID_KAKAO_PLATFORM_ID;
     END IF;
 
     -- 멤버 삽입
@@ -18,6 +20,10 @@ BEGIN
     RETURNING member_id INTO p_member_id;
 
     EXCEPTION
+        WHEN INVALID_MEMBER_NAME THEN
+            RAISE_APPLICATION_ERROR(-20002, 'Invalid member_name: ' || p_member_name);
+        WHEN INVALID_KAKAO_PLATFORM_ID THEN
+            RAISE_APPLICATION_ERROR(-20003, 'Invalid kakao_platform_id: ' || p_kakao_platform_id);
         WHEN OTHERS THEN
             RAISE_APPLICATION_ERROR(-20001, 'Error inserting member: ' || SQLERRM);
 END;
