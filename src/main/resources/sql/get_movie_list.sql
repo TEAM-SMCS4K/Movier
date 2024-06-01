8bbdd5f9934cacee70b2f2e20abb355d2dcd249a
kwonjeong <kwonjeong9470@gmail.com>
2024-06-01 19:05:20 +0900

    [FIX] 영화 리스트 프로시저 변경

--- src/main/resources/sql/get_movie_list.sql
+++ src/main/resources/sql/get_movie_list.sql
@@ -1,21 +1,15 @@
 CREATE OR REPLACE PROCEDURE get_movie_list_cursor (
-  movie_list_cursor OUT SYS_REFCURSOR
+    movie_list_cursor OUT SYS_REFCURSOR
 ) AS
 BEGIN
-OPEN movie_list_cursor FOR
-SELECT
-    m.movie_id AS id,
-    m.movie_name AS title,
-    m.movie_poster_img AS posterImg,
-    NVL(AVG(r.review_rating), 0) AS rating,
-    NVL(COUNT(r.review_id), 0) AS review_count
-FROM
-    movies m
-        LEFT JOIN
-    reviews r
-    ON
-        m.movie_id = r.movie_id
-GROUP BY
-    m.movie_id, m.movie_name, m.movie_poster_img;
+    OPEN movie_list_cursor FOR
+        SELECT
+            movie_id AS id,
+            movie_name AS title,
+            movie_poster_img AS posterImg,
+            movie_review_average_rating AS rating,
+            movie_review_count AS review_count
+        FROM
+            movies;
 END;
 /
