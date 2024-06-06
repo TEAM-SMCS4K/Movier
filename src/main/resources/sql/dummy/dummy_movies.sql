abe548f9f308af0c66a7bf3022f1d41c56e0d0b4
sung-silver <jinna0319@gmail.com>
2024-06-07 03:22:29 +0900

    [CHORE] 더미 스크립트 추가

---
+++ src/main/resources/sql/dummy/dummy_movies.sql
@@ -0,0 +1,366 @@
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (1, '범죄도시4', '액션/범죄', TIMESTAMP '2024-04-24 23:32:04', '109', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88104/88104_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88104/88104224903_727.jpg', '신종 마약 사건 3년 뒤,
+괴물형사 ‘마석도’(마동석)와 서울 광수대는
+배달앱을 이용한 마약 판매 사건을 수사하던 중
+수배 중인 앱 개발자가 필리핀에서 사망한 사건이
+대규모 온라인 불법 도박 조직과 연관되어 있음을 알아낸다.
+
+필리핀에 거점을 두고 납치, 감금, 폭행, 살인 등으로
+대한민국 온라인 불법 도박 시장을 장악한
+특수부대 용병 출신의 빌런 ‘백창기’(김무열)와
+한국에서 더 큰 판을 짜고 있는 IT업계 천재 CEO ‘장동철’(이동휘).
+
+‘마석도’는 더 커진 판을 잡기 위해
+‘장이수’(박지환)에게 뜻밖의 협력을 제안하고
+광역수사대는 물론,
+사이버수사대까지 합류해 범죄를 소탕하기 시작하는데…
+
+나쁜 놈 잡는데 국경도 영역도 제한 없다!
+업그레이드 소탕 작전! 거침없이 싹 쓸어버린다!', 10, 3.9);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (2, '혹성탈출: 새로운 시대', '액션', TIMESTAMP '2024-05-08 20:19:49', '145', 'https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20240508_299%2F1715149331590vuPj9_JPEG%2Fmovie_image.jpg', 'https://search.pstatic.net/common?quality=75&direct=true&src=https%3A%2F%2Fmovie-phinf.pstatic.net%2F20240402_195%2F1712038230270tINuq_JPEG%2Fmovie_image.jpg', '인류의 시대는 끝났고, 세상의 주인이 바뀌었다!
+진화한 유인원과 퇴화된 인간들이 살아가는 땅.
+유인원 리더 ''프록시무스''는 완전한 군림을 위해 인간들을 사냥하며 자신의 제국을 건설한다.
+한편, 또 다른 유인원 ''노아''는 우연히 숨겨진 과거의 이야기와 ‘시저’의 가르침을 듣게 되고,
+의문의 한 인간 소녀와 함께 자유를 향한 여정을 시작하게 되는데…
+', 3, 4.2);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (3, '퓨리오사-매드맥스 사가', '액션', TIMESTAMP '2024-05-22 18:00:27', '148', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88148/88148225266_727.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88148/88148225271_727.jpg', '문명 붕괴 45년 후,
+
+황폐해진 세상 속 누구에게도 알려지지 않은
+
+풍요가 가득한 ‘녹색의 땅’에서 자란 ‘퓨리오사’(안야 테일러-조이)는
+
+바이커 군단의 폭군 ‘디멘투스’(크리스 헴스워스)의 손에 모든 것을 잃고 만다.
+
+
+
+가족도 행복도 모두 빼앗기고 세상에 홀로 내던져진 ‘퓨리오사’는
+
+반드시 고향으로 돌아가겠다는 어머니와의 약속을 지키기 위해
+
+인생 전부를 건 복수를 시작하는데...
+
+
+
+‘매드맥스’ 시리즈의 전설적인 사령관 ‘퓨리오사’의 대서사시
+
+5월 22일, 마침내 분노가 깨어난다!', 3, 3.3);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (4, '설계자', '범죄', TIMESTAMP '2024-05-29 18:05:20', '99', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88220/88220_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88220/88220226131_727.jpg', '“정말 우연이라고 생각해요?”
+
+
+의뢰받은 청부 살인을 사고사로 조작하는 설계자 ‘영일’(강동원)
+
+그의 설계를 통해 우연한 사고로 조작된 죽음들이
+
+실은 철저하게 계획된 살인이라는 것을 아무도 알지 못한다.
+
+
+
+최근의 타겟 역시 아무 증거 없이 완벽하게 처리한 ‘영일’에게
+
+새로운 의뢰가 들어온다.
+
+이번 타겟은 모든 언론과 세상이 주목하고 있는 유력 인사.
+
+작은 틈이라도 생기면 자신의 정체가 발각될 수 있는 위험한 의뢰지만
+
+‘영일’은 그의 팀원인 ‘재키’(이미숙),
+‘월천’(이현욱), ‘점만’(탕준상)과 함께 이를 맡기로 결심한다.
+
+철저한 설계와 사전 준비를 거쳐 마침내 실행에 옮기는 순간
+
+‘영일’의 계획에 예기치 못한 변수가 발생하는데...!
+
+
+
+사고인가
+
+살인인가
+
+그의 실체가 드러나기 시작했다!', 3, 3.3);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (5, '7번방의선물', '코미디', TIMESTAMP '2020-05-21 18:07:48', '127', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000076/76755/76755_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000076/76755/76755102303_727.jpg', '흥행킹 류승룡! 세상에서 가장 사랑스러운 <딸바보>로 돌아오다!
+
+
+
+최악의 흉악범들이 모인 교도소 7번방에 이상한 놈이 들어왔다!
+
+그는 바로 6살 지능의 딸바보 <용구>!
+
+평생 죄만 짓고 살아온 7번방 패밀리들에게 떨어진 미션은 바로 <용구> 딸 <예승>이를 외부인 절대 출입금지인 교도소에 반.입.하.는.것!
+
+
+
+2013년 새해, 웃음과 감동 가득한 사상초유의 합동작전이 시작된다!', 5, 4.6);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (6, '그녀가 죽었다', '스릴러', TIMESTAMP '2024-05-15 18:10:22', '103', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88226/88226_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88226/88226225299_727.jpeg', '“나쁜 짓은 절대 안 해요. 그냥 보기만 하는 거예요.”
+
+고객이 맡긴 열쇠로 그 집에 들어가
+남의 삶을 훔쳐보는 취미를 지닌 공인중개사 ‘구정태’.
+편의점 소시지를 먹으며 비건 샐러드 사진을 포스팅하는
+SNS 인플루언서 ‘한소라’에게 흥미를 느끼고 관찰하기 시작한다.
+
+“관찰 152일째, 그녀가… 죽었습니다.”
+
+급기야 ‘한소라’의 집까지 드나들던 ‘구정태’는
+어느 날, 그녀가 소파에 죽은 채 늘어져 있는 모습을 발견하게 된다.
+
+그 후 그가 ‘한소라’ 집에 들어간 것을 알고 있는 누군가가 협박을 시작하고,
+사건을 맡은 강력반 형사 ‘오영주’의 수사망이 그를 향해 좁혀온다.
+스스로 범인을 찾아야 하는 ‘구정태’는 ‘한소라’의 SNS를 통해
+주변 인물들을 뒤지며 진범을 찾아 나서는데…', 3, 4.5);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (7, '소울메이트', '드라마', TIMESTAMP '2024-05-31 18:12:41', '124', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000086/86884/86884_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000086/86884/86884212701_727.jpg', '“날도 더웠고 수업도 지루했고.. 그렇게 졸리고 나른하던 날에 너를 처음 만났어”
+1998년, 처음 만났다
+
+“누굴 좋아하면 용기내야 된대”
+2004년, 첫사랑이 생겼다
+
+“몰랐어? 나 원래 이렇게 살아”
+2010년, 각자 어른이 되어간다
+
+“가장 그리운 건…너였어”
+2014년, 흔적을 따라간다
+
+“이젠 니 얼굴을 그리고 싶어. 사랑없인 그릴 수조차 없는 그림 말이야”
+지금, 그리움을 그리다
+
+2023년 3월 15일, 당신의 <소울메이트>가 찾아옵니다.', 3, 4.0);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (8, '가필드 더 무비', '애니메이션', TIMESTAMP '2024-05-15 18:46:19', '101', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88183/88183_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88183/88183225204_727.jpg', '상귀찮 집냥이, 바쁘고 험난한 세상에 던져졌다!
+
+집사 ‘존’과 반려견 ‘오디’를 기르며
+평화로운 나날을 보내던 집냥이 ‘가필드’.
+어느 날, 험악한 길냥이 무리에게 납치당해
+냉혹한 거리로 던져진다.
+돌봐주는 집사가 없는 집 밖 세상은
+너무나도 정신없게 돌아가고
+길에서 우연히 다시 만난 아빠 길냥이 ‘빅’은
+오히려 ‘가필드’를 위기에 빠지게 하는데…
+
+험난한 세상, 살아남아야 한다!', 4, 4.3);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (9, '청년경찰', '액션', TIMESTAMP '2017-08-09 18:49:05', '109', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000079/79796/79796_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000079/79796/79796147556_727.jpg', '현장경험 전무, 수사는 책으로 배웠다!
+“그냥 우리가 잡아볼게요”
+
+의욕충만 경찰대생 기준(박서준) X 이론백단 경찰대생 희열(강하늘)
+둘도 없는 친구인 두 사람은 외출을 나왔다 우연히 납치 사건을 목격하게 된다.
+목격자는 오직 두 사람 뿐! 기준과 희열은 학교에서 배운 대로 지체 없이 경찰에 신고한다.
+
+하지만 복잡한 절차와 부족한 증거로 수사는 전혀 진행될 기미가 보이지 않는 상황!
+1분 1초가 급박한 상황에서 아까운 시간만 흘러가자,
+기준과 희열은 직접 발로 뛰는 수사에 나서기로 하고
+예측 불가능한 상황을 마주하게 되는데…
+
+전공지식 총동원! 파릇파릇한 놈들의 혈기왕성 실전수사가 시작된다!', 5, 4.3);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (10, '어벤져스', '액션/SF', TIMESTAMP '2022-10-22 18:53:21', '142', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000076/76249/76249_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000076/76249/7624996496_727.jpg', 'ASSEMBLE! 최강의 슈퍼히어로들이 모였다!
+
+지구의 운명을 건 거대한 전쟁이 시작된다!
+
+지구의 안보가 위협당하는 위기의 상황에서 슈퍼히어로들을 불러모아 세상을 구하는, 일명 [어벤져스] 작전.
+
+에너지원 <큐브>를 이용한 적의 등장으로 인류가 위험에 처하자 국제평화유지기구인 쉴드 (S.H.I.E.L.D)의 국장 닉 퓨리(사무엘 L.잭슨)는
+[어벤져스] 작전을 위해 전 세계에 흩어져 있던 슈퍼히어로들을 찾아나선다.
+
+아이언맨(로버트 다우니 주니어)부터 토르(크리스 헴스워스), 헐크(마크 러팔로), 캡틴 아메리카(크리스 에반스)는 물론,
+쉴드의 요원인 블랙 위도우(스칼렛 요한슨), 호크 아이(제레미 레너)까지, 최고의 슈퍼히어로들이 [어벤져스]의 멤버로 모이게 되지만,
+각기 개성이 강한 이들의 만남은 예상치 못한 방향으로 흘러가는데…
+
+지구의 운명을 건 거대한 전쟁 앞에 [어벤져스] 작전은 성공할 수 있을까?', 4, 4.3);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (11, '감기', '어드벤처', TIMESTAMP '2013-08-14 19:04:34', '121', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000076/76987/76987_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000076/76987/76987106483_727.jpg', '2013 여름, 사상 최악의 바이러스가 대한민국을 덮친다!
+
+호흡기로 감염, 감염속도 초당 3.4명, 치사율 100%의 유례 없는 최악의 바이러스가 대한민국에 발병하고,
+
+이에 정부는 전세계적인 확산을 막기위해 국가 재난사태를 발령,
+
+급기야 도시 폐쇄라는 초유의 결정을 내린다.
+
+피할 새도 없이 격리된 사람들은 일대혼란에 휩싸이게 되고,
+
+대재난 속에서 살아 남기 위한 사람들의 목숨 건 사투가 시작된다.', 5, 3.8);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (12, '파묘', '미스터리', TIMESTAMP '2024-02-22 19:09:54', '134', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88012/88012_320.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88012/88012222949_727.jpg', '미국 LA, 거액의 의뢰를 받은 무당 ‘화림’(김고은)과 ‘봉길’(이도현)은
+기이한 병이 대물림되는 집안의 장손을 만난다.
+조상의 묫자리가 화근임을 알아챈 ‘화림’은 이장을 권하고,
+돈 냄새를 맡은 최고의 풍수사 ‘상덕’(최민식)과 장의사 ‘영근’(유해진)이 합류한다.
+
+“전부 잘 알 거야… 묘 하나 잘못 건들면 어떻게 되는지”
+
+절대 사람이 묻힐 수 없는 악지에 자리한 기이한 묘.
+‘상덕’은 불길한 기운을 느끼고 제안을 거절하지만,
+‘화림’의 설득으로 결국 파묘가 시작되고…
+
+나와서는 안될 것이 나왔다.', 3, 4.5);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (13, '듄-파트2', '액션', TIMESTAMP '2024-02-28 20:24:39', '166', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000087/87947/87947_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000087/87947/87947223177_727.jpg', '황제의 모략으로 멸문한 가문의 유일한 후계자 폴.(티모시 샬라메)
+어머니 레이디 제시카(레베카 퍼거슨)와 간신히 목숨만 부지한 채 사막으로 도망친다.
+그곳에서 만난 반란군들과 숨어 지내다 그들과 함께 황제의 모든 것을 파괴할 전투를 준비한다.
+한편 반란군들의 기세가 높아질수록 불안해진 황제와 귀족 가문은 잔혹한 암살자 페이드 로타(오스틴 버틀러)를 보내 반란군을 몰살하려 하는데…', 3, 4.8);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (14, '쿵푸팬더4 ', '애니메이션', TIMESTAMP '2024-04-10 20:29:02', '94', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000087/87948/87948_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000087/87948/87948224030_727.jpg', '오랜만이지! 드림웍스 레전드 시리즈 마침내 컴백!
+
+마침내 내면의 평화… 냉면의 평화…가 찾아왔다고 믿는 용의 전사 ‘포’
+이젠 평화의 계곡의 영적 지도자가 되고, 자신을 대신할 후계자를 찾아야만 한다.
+“이제 용의 전사는 그만둬야 해요?”
+
+용의 전사로의 모습이 익숙해지고 새로운 성장을 하기보다 지금 이대로가 좋은 ‘포’
+하지만 모든 쿵푸 마스터들의 능력을 그대로 복제하는 강력한 빌런 ‘카멜레온’이 나타나고
+그녀를 막기 위해 정체를 알 수 없는 쿵푸 고수 ‘젠’과 함께 모험을 떠나게 되는데…
+포는 가장 강력한 빌런과 자기 자신마저 뛰어넘고 진정한 변화를 할 수 있을까?', 4, 4.0);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (15, '하이큐!! 끝과 시작', '애니메이션', TIMESTAMP '2024-05-11 20:32:43', '89', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88233/88233_1000.jpg', 'https://image.tving.com/ntgs/contents/CTC/caim/CAIM2400/com/20220104/M000365246.jpg/dims/resize/1280', '“코트 최후의 승자는 바로 우리야!”
+
+‘한 때’ 고교 배구의 명문이었던 카라스노 고교에
+입학하게 된 의욕 충만한 스파이커 ‘히나타’.
+
+우연히 중학 시절 시합에서 만났던 천재 세터
+‘카게야마’를 같은 학교 배구부에서 만나고,
+라이벌 구도를 형성하지만 점차 호흡을 맞추게 된다.
+
+카라스노 고교 배구부는 예전의 명성을 찾기 위해
+점차 팀을 꾸리게 되고 영원한 라이벌 네코마 고교와의
+본격적인 첫 공식 연습 경기를 가지게 되는데…
+
+최강이 되기 위한 불꽃 같은 승부가 시작된다!', 3, 3.8);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (16, '남은 인생 10년 ', '로맨스/멜로', TIMESTAMP '2024-04-03 20:37:23', '125', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88126/88126_1000.jpg', 'https://i.namu.wiki/i/B9xTrfPCcWjEn108Q50UdUVc2o0OaVckG1rNXGysqD_HnZpGhxkzCRPoeszEZsgKNVZuSsWKIe8phIiln1jsHHYHD45futE8o8v9Hk5P9xZNKIZA-Q3RxtDQ1CqAob0NpUFmSnJbPxtSPz7fLKOhUA.webp', '매일 밤, 눈 감기가 두려웠던 난치병 환자 ‘마츠리’
+매일 아침, 눈 뜨기가 두려웠던 외톨이 ‘카즈토’
+운명은 정반대에 있던 두 사람을 마주 보게 한다.
+
+처음 만난 봄,
+많이 웃은 여름,
+더욱 깊어진 가을,
+소중해서 아팠던 겨울까지
+남은 인생을 진정으로 사랑하게 해준 너에게 감사해', 4, 4.3);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (17, '드림 시나리오', '코미디', TIMESTAMP '2024-05-29 20:42:41', '102', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88168/88168_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88168/88168225276_727.jpg', '소심하고, 한심하고, 평범 그 자체여서
+언제 어디서나 존재감 없는 ‘폴’로 인해 온 세상이 떠들썩해진다!
+
+왜? 그가 지구상 모두의 꿈에 나타나고 있기 때문에!
+
+“실존 인물 맞나요? 왜 당신 꿈을 꾸죠? 도대체 누구세요?”
+SNS 메시지 폭주, 인터뷰 출연, 광고 모델 요청은 물론,
+심지어 꿈속 만남이 현실로 이어지는 기막힌 일까지!
+
+꿈속 남자에서 모두가 꿈꾸는 남자로 거듭난 ‘폴’!
+하지만 갑자기 그가 등장하는 모든 꿈들이 악몽이 되는데…', 4, 4.3);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (18, '원더랜드', '드라마', TIMESTAMP '2024-06-05 16:22:56', '113', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88266/88266_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88266/88266225728_727.jpg', '언제 어디서든 다시 만날 수 있습니다
+
+죽은 사람을 인공지능으로 복원하는
+‘원더랜드’ 서비스가 일상이 된 세상,
+어린 딸에게 자신의 죽음을 숨기기 위해
+‘원더랜드’ 서비스를 의뢰한 ‘바이리’와
+사고로 누워있는 남자친구 ‘태주’를 ‘원더랜드’에서
+우주인으로 복원해 행복한 일상을 나누는 ‘정인’.
+사람들은 더 이상
+그리워하거나 슬퍼하지 않는 삶을 선택할 수 있게 된다.
+
+‘원더랜드’의 수석 플래너 ‘해리’와 신입 플래너 ‘현수’는
+‘원더랜드’를 찾는 사람들이 소중한 기억을
+이어갈 수 있도록 바쁜 하루하루를 보낸다.
+어린 시절부터 인공지능 부모님과 교감해온 ‘해리’는
+이용자들의 상황을 더욱 세심히 살피고,
+‘현수’는 의뢰받은 서비스에서
+뜻밖의 비밀을 발견하게 되어 마음이 쓰인다.
+
+어느 날 의식불명 상태의 ‘태주’가
+기적처럼 깨어나 ‘정인’ 곁으로 돌아오고
+다시 마주하게 된 모든 것이 낯설고
+혼란스러운 ‘태주’와
+그런 그와 함께하는 현실에
+‘정인’의 마음에는 조금씩 균열이 찾아온다.
+
+한편, ‘원더랜드’에서 발굴 현장을 누비는
+고고학자로 복원된 ‘바이리’는
+딸과의 영상통화를 통해
+친구 같은 엄마가 되기 위해 노력하지만
+갑작스럽게 서비스가 종료되면서
+예상치 못한 오류가 발생하는데…', 4, 4.5);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (19, '헤어질 결심', '로맨스/멜로', TIMESTAMP '2022-06-29 16:27:16', '138', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000085/85852/85852_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000085/85852/85852203821_727.jpg', '산 정상에서 추락한 한 남자의 변사 사건.
+담당 형사 ''해준''(박해일)은
+사망자의 아내 ''서래''(탕웨이)와 마주하게 된다.
+
+"산에 가서 안 오면 걱정했어요, 마침내 죽을까 봐."
+
+남편의 죽음 앞에서 특별한 동요를 보이지 않는 ''서래''.
+경찰은 보통의 유가족과는 다른 ''서래''를 용의선상에 올린다.
+''해준''은 사건 당일의 알리바이 탐문과 신문,
+잠복수사를 통해 ''서래''를 알아가면서
+그녀에 대한 관심이 점점 커져가는 것을 느낀다.
+
+한편, 좀처럼 속을 짐작하기 어려운 ‘서래’는
+상대가 자신을 의심한다는 것을 알면서도
+조금의 망설임도 없이 ‘해준’을 대하는데….
+
+진심을 숨기는 용의자
+용의자에게 의심과 관심을 동시에 느끼는 형사
+그들의 <헤어질 결심>', 0, 0.0);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (20, '이터널 선샤인', '로맨스/멜로 ', TIMESTAMP '2018-01-03 16:29:29', '107', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000078/78586/78586_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000078/78586/78586131734_727.jpg', '사랑은 그렇게 다시 기억된다..
+
+
+
+조엘은 아픈 기억만을 지워준다는 라쿠나사를 찾아가 헤어진 연인 클레멘타인의 기억을 지우기로 결심한다.
+
+기억이 사라져 갈수록 조엘은 사랑이 시작되던 순간, 행복한 기억들, 가슴 속에 각인된 추억들을 지우기 싫어지기만 하는데...
+
+당신을 지우면 이 아픔도 사라질까요?
+
+사랑은 그렇게 다시 기억된다.', 0, 0.0);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (21, '어바웃 타임', '코미디', TIMESTAMP '2021-01-28 16:31:25', '123', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000077/77305/77305_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000077/77305/77305109027_727.jpg', '모태솔로 팀(돔놀 글리슨)은 성인이 된 날, 아버지(빌 나이)로부터 놀랄만한 가문의 비밀을 듣게 된다.
+
+바로 시간을 되돌릴 수 있는 능력이 있다는 것!
+
+그것이 비록 히틀러를 죽이거나 여신과 뜨거운 사랑을 할 수 는 없지만, 여자친구는 만들어 줄 순 있으리..
+
+
+
+꿈을 위해 런던으로 간 팀은 우연히 만난 사랑스러운 여인 메리에게 첫눈에 반하게 된다.
+
+그녀의 사랑을 얻기 위해 자신의 특별한 능력을 마음껏 발휘하는 팀.
+
+어설픈 대시, 어색한 웃음은 리와인드! 뜨거웠던 밤은 더욱 뜨겁게 리플레이!
+
+꿈에 그리던 그녀와 매일매일 최고의 순간을 보낸다.
+
+
+
+하지만 그와 그녀의 사랑이 완벽해 질수록 팀을 둘러싼 주변 상황들은 미묘하게 엇갈리고,
+
+예상치 못한 사건들이 여기저기 나타나기 시작하는데…
+
+
+
+어떠한 순간을 다시 살게 된다면, 과연 완벽한 사랑을 이룰 수 있을까?', 0, 0.0);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (22, '인터스텔라', 'SF', TIMESTAMP '2022-10-05 16:33:03', '169', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000077/77372/77372_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000077/77372/77372123386_727.jpg', '“우린 답을 찾을 거야, 늘 그랬듯이”
+
+세계 각국의 정부와 경제가 완전히 붕괴된 미래가 다가온다.
+
+지난 20세기에 범한 잘못이 전 세계적인 식량 부족을 불러왔고, NASA도 해체되었다.
+
+이때 시공간에 불가사의한 틈이 열리고, 남은 자들에게는 이 곳을 탐험해 인류를 구해야 하는 임무가 지워진다.
+
+사랑하는 가족들을 뒤로 한 채 인류라는 더 큰 가족을 위해, 그들은 이제 희망을 찾아 우주로 간다.
+
+그리고 우린 답을 찾을 것이다. 늘 그랬듯이…', 1, 4.5);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (23, '해리포터와 불사조기사단', '어드벤처', TIMESTAMP '2023-09-06 16:35:39', '153', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000087/87295/87295_1000.jpg', 'https://mblogthumb-phinf.pstatic.net/MjAyMDAzMDhfMjI0/MDAxNTgzNjU2NjY4MTQz.KmXXy5R2spW4vI_01Gj0bfnewkwLJQCxVlchgpS5-WUg.Iok6KuhRYDizneQ5lgKKzitglwPU3n64_1-MfEmXXsMg.JPEG.hildaz/%EC%8A%A4%ED%8B%B8%EC%BB%B720.jpg?type=w800', '호그와트 최대 위기! 볼드모트 그가 돌아왔다!
+어둠의 세력에 빠진 호그와트를 구하기 위해 해리포터와 불사조 기사단, 그들이 손을 잡았다!
+
+
+호그와트 마법학교에서 5번째 해를 맞이하게 된 해리포터(다니엘 래드클리프).
+헤르미온느(엠마 왓슨)와 론(루퍼트 그린트)과의 관계도 소원하고 예언자 일보는 해리에 대해 악의적인 소문을 퍼뜨려 해리를 곤경에 빠뜨린다.
+초챙(케이티 렁)과의 로맨스도 힘에 겹다.
+게다가 마법부는 볼드모트(랄프 파인즈)가 돌아왔다는 이야기를 애써 모른 체 하려 하고
+오히려 호그와트 교장 덤블도어가 헛소문을 퍼트려 마법부의 권위를 실추시킨다며 비난한다.
+여기에 마법부 장관 코넬리우스 퍼지는 새로운 어둠의 방어술 교수 자리에 자신의 심복 돌로레스 엄브리지를 임명해 덤블도어와 학생들을 감시하게 한다.
+형편없는 그의 수업은 어둠의 마법에 대한 학생들의 방어능력을 한심한 수준으로 격하시킨다.
+
+
+이제 해리포터는 호그와트로 다시 돌아온 악의 세력 볼드모트에 대항하기 위해 만들어진
+마법사들의 비밀단체 불사조기사단과 함께 볼드모트와 죽음의 대결은 준비한다.
+그 와중에 해리는 소중한 사람의 죽음까지 맞이하는 아픔을 겪게 되는데…
+
+
+위기에 빠진 호그와트!
+해리포터와 불사조 기사단, 모든 것이 그들 손에 달렸다!!', 0, 0.0);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (24, '가디언즈 오브 갤럭시', '어드벤처', TIMESTAMP '2017-04-20 16:47:40', '121', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000077/77347/77347_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000077/77347/77347119643_727.jpg', '자칭 전설의 무법자 스타로드, 그러나 현실은 우주를 떠도는 그저그런 좀도둑에 불과한 피터 퀼(크리스 프랫).
+뜻하지 않게 갤럭시의 절대악 타노스와 로난의 타겟이 된 그는 감옥에서 만난 암살자 가모라(조 샐다나),
+거구의 파이터 드랙스(데이브 바티스타), 현상금 사냥꾼 로켓(브래들리 쿠퍼)과 그루트(빈 디젤) 콤비와
+불편한 동맹을 맺고 일명 ‘가디언즈 오브 갤럭시’를 결성한다.
+범상치 않은 화려한 과거를 지닌 이들이 과연 120억 명의 운명을 구할 유일한 희망이 될 수 있을까?
+
+흩어지면 무법자, 뭉치면 히어로
+차원이 다른 마블의 새로운 세계를 목격하라!
+', 0, 0.0);
+INSERT INTO MOVIER_ADMIN.MOVIES (MOVIE_ID, MOVIE_NAME, MOVIE_GENRE, MOVIE_RELEASE_DATE, MOVIE_RUNNING_TIME, MOVIE_POSTER_IMG, MOVIE_THUMBNAIL_IMG, MOVIE_PLOT, MOVIE_REVIEW_COUNT, MOVIE_REVIEW_AVERAGE_RATING) VALUES (25, '존 오브 인터레스트', '드라마', TIMESTAMP '2024-06-05 16:50:55', '105', 'https://img.cgv.co.kr/Movie/Thumbnail/Poster/000088/88270/88270_1000.jpg', 'https://img.cgv.co.kr/Movie/Thumbnail/StillCut/000088/88270/88270225996_727.jpg', '독일 장교 루돌프 회스(크리스티안 프리델)의 가족이 사는
+그들만의 꿈의 왕국 아우슈비츠.
+아내 헤트비히(산드라 휠러)가 정성스럽게 가꾼 꽃이 만발한 정원에는
+재잘거리는 아이들의 웃음소리로 가득하다.
+마치 한 폭의 그림 같은 집.
+과연 악마는 다른 세상을 사는가?', 0, 0.0);
